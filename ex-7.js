d2f92284edbb6b153b0ef7bb355c56ab4161a35d
hedisnice <nicenice60225@gmail.com>
2023-11-29 10:50:53 +0700

    feat(exercise): add complete exercise

--- ex-7.js
+++ ex-7.js
@@ -1,6 +1,9 @@
 //Start coding here
-const greeting = "Hello ! 😁";
-
+//const greeting = "Hello ! 😁";
+//greeting เป็น string เขียนเป็น function ไม่ได้ ต้องเปลี่ยน const เป็น function แล้วใส่ () หลัง greeting
+function greeting() {
+  console.log("Hello ! 😁");
+}
 greeting();
 
 console.log("This line will be executed without any error.");
